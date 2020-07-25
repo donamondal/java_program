aa329251a974f059180dfe0e97647a76572dacfd
donamondal <donamondal25@gmail.com>
2020-07-25 19:47:55 +0530

    ArrayUtils

--- integers limiy/src/pkg1/Array_Utils.java
+++ integers limiy/src/pkg1/Array_Utils.java
@@ -3,6 +3,7 @@ package pkg1;
 public class Array_Utils 
 {
 
+//	CHECKING A ARRAY IS SORTED OR NOT 
 	public static boolean is_sorted(int[] array) {
 		for (int i = 0; i < array.length - 1; i++) {
 			if (array[i] > array[i + 1]) {
@@ -11,7 +12,8 @@ public class Array_Utils
 		}
 		return true;
 	}
-
+	
+//	SORTING AN ARRAY
 	public static int[] sorting(int[] a)
 	{
 		int t=0;
@@ -30,4 +32,91 @@ public class Array_Utils
 	  return a;
 	}
 	
+//	FINDING MAX NO. IN A 1D ARRAY	
+	public static int maximum(int[] array)
+	{
+       int t=array[0];
+		
+		for(int i=0;i<array.length-1;i++)
+		{
+			if(t<array[i])
+			{
+				t=array[i];
+			}
+		}
+		return t;
+	}
+	
+//	FINDING MAX NO. IN A 2D ARRAY
+	public static int maximum(int[][] a)
+	{
+		int large=a[0][0];
+		 
+		for(int r=0;r<a.length;r++)
+		{
+			for(int c=0;c<a[r].length;c++)
+			{
+				if(large<a[r][c])
+				{
+					large=a[r][c];
+				}
+			}
+		}
+		return large;
+	}
+	
+//	FINDING MIN NO. IN A 1D ARRAY
+	public static int smallest(int[] array)
+	{
+		int temp=0,i=0;
+		temp=array[i];
+		   for(i=0;i<array.length;i++)
+		  {	
+             if(temp>array[i])
+             {
+            	 temp=array[i];
+             }
+		  }
+		return temp;
+	}
+	
+//	FINDING MIN NO. IN A 2D ARRAY
+	public static int Smallest(int[][] a)
+	{
+
+		int min=0,r=0,c=0;
+		min=a[r][c];
+		for( r=0;r<a.length;r++)
+		{
+			for(c=0;c<a[r].length;c++)
+			{
+				if(min>a[r][c])
+				{
+					min=a[r][c];
+				}
+			}
+		}
+		return min;
+	}
+	
+//	FINDING THE SUM OF ELEMENTS OF 1D ARRAY
+	public static int sum(int[] a) {
+		int sum = 0;
+		for (int i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+		return sum;
+	}
+	
+//	FINDING THE SUM OF 
+	public static int sum(int[][] array) {
+		int total = 0;
+		for (int r = 0; r < array.length; r++) {
+			for (int c = 0; c < array[r].length; c++) {
+				total += array[r][c];
+			}
+		}
+		return total;
+	}
+
 }	
